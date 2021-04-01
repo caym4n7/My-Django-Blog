e1d775a04ef7443c12f06e1b3c7eca92ded26298
Eventuri7 <crosser231@gmail.com>
2021-04-01 19:20:36 +0300

    password reset

--- myblog/settings.py
+++ myblog/settings.py
@@ -151,3 +151,10 @@ X_FRAME_OPTIONS = 'SAMEORIGIN'      # Summernote editor
 SUMMERNOTE_THEME = 'bs4'            # Summernote editor
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4' # Crispy forms
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
